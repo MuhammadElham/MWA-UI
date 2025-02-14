a3c3fef774a43e541eee6cbdb50a0021a2465319
Elham <elhamwaheed777@gmail.com>
2025-02-15 03:07:43 +0500

    Making Card

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -5,18 +5,20 @@ import { FaMinus, FaPlus } from "react-icons/fa";
 const Card1 = () => {
   return (
     <>
-      <div className="main_container flex justify-center items-center w-full h-screen bg-gradient-to-r from-sky-200 to-blue-400">
-        <div className="container w-full max-w-[500px] py-[30px] px-[25px] rounded-lg border-none shadow-md bg-white">
+      <div className="main_container flex justify-center items-center w-full min-h-screen bg-gradient-to-r from-sky-200 to-blue-400">
+        {/* Card container */}
+        <div className="container w-full max-w-[90%] sm:max-w-[500px] py-[30px] px-[25px] sm:py-[50px] sm:px-[45px] rounded-3xl border-none shadow-md bg-white">
+          {/* Part 1 */}
           <div className="part_1">
-            <h2 className="font-bold text-2xl">Your shopping cart</h2>
-            <p className="text-base">ID: 548856</p>
+            <h2 className="font-bold text-xl sm:text-2xl">Your shopping cart</h2>
+            <p className="text-sm sm:text-base mt-1">ID: 548856</p>
           </div>
-
+          {/* Part 2 */}
           <div className="part_2 md:flex items-start gap-8 my-6">
             <div className="product1_img flex justify-center items-center">
-              <img className="w-[140px] md:h-[80px]" src={product_1} />
+              <img className="w-[140px] sm:w-[140px] h-auto" src={product_1} />
             </div>
-            <div>
+            <div className="mt-6 md:mt-0">
               <p className="font-semibold">
                 Ecovacs Deebot OZMO N8 + robot vacuum cleaner W125978483
               </p>
@@ -33,7 +35,8 @@ const Card1 = () => {
               </div>
             </div>
           </div>
-          <hr className="border-[1px]"/>
+          <hr className="border-[1px]" />
+          {/* Part 3 */}
           <div className="part_3">
             <div className="flex items-center justify-between mt-6">
               <p className="tracking-wide">Standard package</p>
@@ -47,7 +50,12 @@ const Card1 = () => {
               <p className="tracking-wide font-semibold">Total NOK</p>
               <p className="font-semibold">4395,-</p>
             </div>
-            <p className="mt-4 text-sky-600">Got a gift card or a promotional code?</p>
+            <p className="mt-4 text-sky-600 underline">
+              Got a gift card or a promotional code?
+            </p>
+            <button className="text-white bg-blue-500 w-full text-center mt-6 rounded-3xl px-4 py-3 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-blue-600">
+              Checkout
+            </button>
           </div>
         </div>
       </div>
