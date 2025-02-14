0427575f09e72ad62688dca3b86b98ca5dc07495
Elham <elhamwaheed777@gmail.com>
2025-02-15 02:28:40 +0500

    working on link

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -5,19 +5,16 @@ import { FaMinus, FaPlus } from "react-icons/fa";
 const Card1 = () => {
   return (
     <>
-      <div className="main_container flex justify-center items-center w-full h-screen">
-        <div className="container w-full max-w-[500px] p-[20px] rounded-lg border-none shadow-md">
+      <div className="main_container flex justify-center items-center w-full h-screen bg-gradient-to-r from-sky-200 to-blue-400">
+        <div className="container w-full max-w-[500px] py-[30px] px-[25px] rounded-lg border-none shadow-md bg-white">
           <div className="part_1">
             <h2 className="font-bold text-2xl">Your shopping cart</h2>
             <p className="text-base">ID: 548856</p>
           </div>
 
-          <div className="part_2 md:flex items-center gap-8">
-            <div className="product1_img flex justify-center items-center my-6">
-              <img
-                className="w-[220px] md:h-[130px]"
-                src={product_1}
-              />
+          <div className="part_2 md:flex items-start gap-8 my-6">
+            <div className="product1_img flex justify-center items-center">
+              <img className="w-[140px] md:h-[80px]" src={product_1} />
             </div>
             <div>
               <p className="font-semibold">
@@ -36,7 +33,22 @@ const Card1 = () => {
               </div>
             </div>
           </div>
-          <div className="part_3"></div>
+          <hr className="border-[1px]"/>
+          <div className="part_3">
+            <div className="flex items-center justify-between mt-6">
+              <p className="tracking-wide">Standard package</p>
+              <p className="font-semibold">FREE</p>
+            </div>
+            <div className="flex items-center justify-between mt-4">
+              <p className="tracking-wide">Total VAT</p>
+              <p className="font-semibold">879,-</p>
+            </div>
+            <div className="flex items-center justify-between mt-4">
+              <p className="tracking-wide font-semibold">Total NOK</p>
+              <p className="font-semibold">4395,-</p>
+            </div>
+            <p className="mt-4 text-sky-600">Got a gift card or a promotional code?</p>
+          </div>
         </div>
       </div>
     </>
