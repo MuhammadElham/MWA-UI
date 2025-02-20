dd7f4b6090132dddc909d61a6bfdaaecdd324cdb
Elham <elhamwaheed777@gmail.com>
2025-02-20 18:20:29 +0500

    component3 Done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const Form = () => {
+  const nameRef = useRef();
+  const descRef = useRef();
+  const priceRef = useRef();
+  const categRef = useRef();
+  const stockRef = useRef();
+  const quantRef = useRef();
+
+  const [submit, setSubmit] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const entry = {
+      name: nameRef.current.value,
+      description: descRef.current.value,
+      price: priceRef.current.value,
+      category: categRef.current.value,
+      stock: stockRef.current.value,
+      quantity: quantRef.current.value,
+    };
+    setSubmit((submit) => {
+      const updateArray = [...submit, entry];
+      console.log("Update Array = ", updateArray);
+      return updateArray;
+    });
+  }
+
   return (
     <div className="main-container flex items-center justify-center w-full min-h-screen bg-gradient-to-r from-sky-200 to-blue-400 p-4">
-      <div className="container w-full max-w-[500px] sm:max-w-[600px] py-8 px-6 sm:px-12 bg-white rounded-3xl shadow-lg">
+      <form
+        onSubmit={handleSubmit}
+        className="container w-full max-w-[500px] sm:max-w-[600px] py-8 px-6 sm:px-12 bg-white rounded-3xl shadow-lg"
+      >
         {/* 1 */}
         <div className="w-full flex flex-col sm:flex-row sm:items-center gap-2 mb-2 sm:mb-4">
           <label htmlFor="name" className="font-semibold text-gray-700">
@@ -12,6 +41,8 @@ const Form = () => {
           <input
             type="text"
             id="name"
+            name="name"
+            ref={nameRef}
             placeholder="Enter product name"
             className="border border-sky-400 rounded-md outline-none py-2 px-3 w-full flex-1"
           />
@@ -24,6 +55,8 @@ const Form = () => {
           <input
             type="text"
             id="name"
+            name="description"
+            ref={descRef}
             placeholder="Enter product description "
             className="border border-sky-400 rounded-md outline-none py-2 px-3 w-full flex-1"
           />
@@ -36,6 +69,8 @@ const Form = () => {
           <input
             type="text"
             id="name"
+            name="price"
+            ref={priceRef}
             placeholder="Enter product price"
             className="border border-sky-400 rounded-md outline-none py-2 px-3 w-full  flex-1"
           />
@@ -48,6 +83,8 @@ const Form = () => {
           <input
             type="text"
             id="name"
+            name="category"
+            ref={categRef}
             placeholder="Enter product category "
             className="border border-sky-400 rounded-md outline-none py-2 px-3 w-full  flex-1"
           />
@@ -60,12 +97,14 @@ const Form = () => {
           <input
             type="text"
             id="name"
+            name="stock"
+            ref={stockRef}
             placeholder="Enter product stock"
             className="border border-sky-400 rounded-md outline-none py-2 px-3 w-full flex-1"
           />
         </div>
         {/* 6 */}
-        <div className="w-full flex flex-col sm:flex-row sm:items-center gap-2 mb-2 sm:mb-4">
+        {/* <div className="w-full flex flex-col sm:flex-row sm:items-center gap-2 mb-2 sm:mb-4">
           <label htmlFor="name" className="font-semibold text-gray-700">
             Product Image:
           </label>
@@ -75,20 +114,26 @@ const Form = () => {
             placeholder="Enter product image"
             className="border border-sky-400 rounded-md outline-none py-2 px-3 w-full flex-1"
           />
-        </div>
+        </div> */}
         {/* 7 */}
         <div className="w-full flex flex-col sm:flex-row sm:items-center gap-2 sm:mb-4">
           <label htmlFor="name" className="font-semibold text-gray-700">
-            Product Price:
+            Product Quantity:
           </label>
           <input
             type="text"
             id="name"
-            placeholder="Enter product price"
+            name="quantity"
+            ref={quantRef}
+            placeholder="Enter product quantity"
             className="border border-sky-400 rounded-md outline-none py-2 px-3 w-full flex-1"
           />
         </div>
-      </div>
+        {/* Button */}
+        <button className="text-lg bg-blue-400 text-white py-1 px-3 rounded hover:bg-blue-500 duration-150 mt-4 w-full sm:mt-0 sm:text-xl sm:w-auto">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
